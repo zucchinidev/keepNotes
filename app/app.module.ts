63516999261a944344c4dd298d6a8090a3d15e2c
Andrea Zucchini <azga84@gmail.com>
2016-09-18 23:57:52 +0200

    Add service to the main module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import './rxjs-extensions';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 
 import {AppRootComponent} from './root/app-root.component';
@@ -9,11 +10,16 @@ import {MainContainerComponent} from './main-container/main-container.component'
 import {AppBarComponent, NoteCardComponent, NoteCreatorComponent} from './ui/index';
 import {NotesComponent} from './notes/notes.component';
 import {ColorPickerComponent} from './ui/color-picker/color-picker.component';
+import {InMemoryWebApiModule} from 'angular2-in-memory-web-api';
+import {InMemoryDataService} from './services/in-memory-data.service';
+import {NoteService} from './services/note.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppRootComponent,
@@ -24,7 +30,9 @@ import {ColorPickerComponent} from './ui/color-picker/color-picker.component';
     NoteCreatorComponent,
     ColorPickerComponent
   ],
-  providers: [],
+  providers: [
+    NoteService
+  ],
   bootstrap: [
     AppRootComponent
   ]
