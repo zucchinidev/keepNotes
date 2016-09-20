97efab7832aa55437554b5041d6e602c53b574e0
Andrea Zucchini <azga84@gmail.com>
2016-09-20 22:04:26 +0200

    about container component styles and html

--- app/about-container/about-container.component.ts
+++ app/about-container/about-container.component.ts
@@ -1,1 +1,11 @@
+import {Component} from '@angular/core';
 
+
+@Component({
+  selector: 'about-container',
+  templateUrl: 'app/about-container/about-container.component.html',
+  styleUrls: ['app/about-container/about-container.component.css']
+})
+export class AboutContainerComponent {
+
+}
