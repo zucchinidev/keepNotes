ca37a0b8040a09d9fb75cecb8c270c2aca313b0d
Andrea Zucchini <azga84@gmail.com>
2016-09-20 22:05:33 +0200

    Create router

--- app/config/router/router.ts
+++ app/config/router/router.ts
@@ -1,3 +1,30 @@
-/**
- * Created by Andrea on 20/09/2016.
- */
+import {NotesComponent} from '../../notes/notes.component';
+import {Routes, RouterModule} from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+import {MainContainerComponent} from '../../main-container/main-container.component';
+import {AboutContainerComponent} from '../../about-container/about-container.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: MainContainerComponent,
+    children: [
+      {
+        path: '',
+        component: NotesComponent
+      },
+      {
+        path: 'about',
+        component: AboutContainerComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, {
+  useHash: true
+});
