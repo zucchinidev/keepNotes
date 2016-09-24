ce015c5c8b55d6c73f353dc5d316b6e37d3238e5
Andrea Zucchini <azga84@gmail.com>
2016-09-24 09:56:59 +0200

    Create auth container component

--- app/auth-container/auth-container.component.ts
+++ app/auth-container/auth-container.component.ts
@@ -1,3 +1,12 @@
-/**
- * Created by Andrea on 24/09/2016.
- */
+import {Component} from '@angular/core';
+
+
+@Component({
+  'selector': 'auth-container',
+  templateUrl: 'app/auth-container/auth-container.component.html',
+  styleUrls: ['app/auth-container/auth-container.component.css']
+})
+export class AuthContainerComponent {
+
+}
+
