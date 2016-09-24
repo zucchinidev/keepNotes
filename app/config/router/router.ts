cba4c67d96506f4238de18dcd346d18b481dee52
Andrea Zucchini <azga84@gmail.com>
2016-09-24 09:57:15 +0200

    add auth router

--- app/config/router/router.ts
+++ app/config/router/router.ts
@@ -3,11 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import {MainContainerComponent} from '../../main-container/main-container.component';
 import {AboutContainerComponent} from '../../about-container/about-container.component';
+import {AuthContainerComponent} from '../../auth-container/auth-container.component';
+import {AuthService} from '../../services/auth.service';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: MainContainerComponent,
+    canActivate: [AuthService],
     children: [
       {
         path: '',
@@ -19,10 +22,8 @@ const appRoutes: Routes = [
       }
     ]
   },
-  {
-    path: '**',
-    redirectTo: ''
-  }
+  {path: 'auth', component: AuthContainerComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, {
