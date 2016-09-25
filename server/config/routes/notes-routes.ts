45242439ac645a7b260430dd9b03450c32d6985c
Andrea Zucchini <azga84@gmail.com>
2016-09-25 19:26:49 +0200

    Remove static data

--- server/config/routes/notes-routes.ts
+++ server/config/routes/notes-routes.ts
@@ -1,43 +1,6 @@
 import * as express from 'express';
 import {NoteController} from '../../app/controllers/note.controller';
 
-const notes = [{
-  id: 1,
-  title: 'new note 1',
-  value: 'note here',
-  color: 'seagreen'
-}, {
-  id: 2,
-  title: 'new note 2',
-  value: 'note here',
-  color: 'tomato'
-}, {
-  id: 3,
-  title: 'new note 3',
-  value: 'note here',
-  color: 'olive'
-}, {
-  id: 4,
-  title: 'new note 4',
-  value: 'note here',
-  color: 'blue'
-}, {
-  id: 5,
-  title: 'new note 5',
-  value: 'note here',
-  color: 'grey'
-}, {
-  id: 6,
-  title: 'new note 6',
-  value: 'note here',
-  color: 'red'
-}, {
-  id: 7,
-  title: 'new note 7',
-  value: 'note here',
-  color: 'yellow'
-}];
-
 export class NotesRoutes {
   private router: express.Router;
   constructor(private controller = new NoteController()) {
