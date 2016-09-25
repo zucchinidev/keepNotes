b1263cc953e899520002b2789b0789a9cca46038
Andrea Zucchini <azga84@gmail.com>
2016-09-25 19:17:49 +0200

    Add base business interfaces

---
+++ server/app/business/interfaces/base/base-business.interface.ts
@@ -0,0 +1,9 @@
+import {IReadBusiness} from '../common/read-business.interface';
+import {IWriteBusiness} from '../common/wirte-business.interface';
+import {IDocument} from '../../../model/interfaces/document.interface';
+import {BaseRepository} from '../../../repository/base/base-repository';
+
+
+export interface IBaseBusiness<T> extends IReadBusiness<T>, IWriteBusiness<T> {
+  repository: BaseRepository<IDocument>;
+}
