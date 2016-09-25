a4fe376a4692f08c460f7b0425970e6048d07e17
Andrea Zucchini <azga84@gmail.com>
2016-09-25 19:13:06 +0200

    Add middlewares base

---
+++ server/libs/middlewares/base/middlewares.ts
@@ -0,0 +1,38 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import {MethodOverride} from '../methodOverride';
+import {BaseRoutes} from '../../../config/routes/base/baseRoutes';
+
+type BodyParseMethod = express.RequestHandler;
+
+export class Middlewares {
+
+  static defaultConfiguration(): express.Express {
+    const app = express();
+    const methods = Middlewares.getBodyParseMethods();
+    methods.forEach((method: BodyParseMethod) => app.use(method));
+    app.use(MethodOverride.override());
+    app.use('/', Middlewares.getLoggerMiddleware());
+    app.use('/', BaseRoutes.routes());
+    return app;
+  }
+
+  private static getBodyParseMethods(): BodyParseMethod[] {
+    return [
+      bodyParser.urlencoded({
+        extended: true
+      }),
+      bodyParser.json()
+    ];
+  }
+
+  private static getLoggerMiddleware(): express.RequestHandler {
+    return (req: express.Request, res: express.Response, next: Function) => {
+      console.log(`Request ${req.url} - method: ${req.method}`);
+      if (req.body && Object.keys(req.body).length > 0) {
+        console.log(`Body: ${JSON.stringify(req.body)}`);
+      }
+      next();
+    };
+  }
+}
