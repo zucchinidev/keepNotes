62cde04de877d8dc5c2e3ebd2d9f871f1e46a9f5
Andrea Zucchini <azga84@gmail.com>
2016-09-25 19:21:21 +0200

    add note mongoose model

---
+++ server/app/dataAccess/mongoose-models/note.mongoose-model.ts
@@ -0,0 +1,14 @@
+import * as mongoose from 'mongoose';
+import {INote} from '../../model/interfaces/note.interface';
+import {NoteScheme} from '../mongoose-schemas/note.sheme';
+
+export class NoteMongooseModel {
+  public static model: mongoose.Model<INote>;
+
+  static getModel(): mongoose.Model<INote> {
+    if (typeof NoteMongooseModel.model === 'undefined') {
+      NoteMongooseModel.model = mongoose.model<INote>('Note', new NoteScheme().schema);
+    }
+    return NoteMongooseModel.model;
+  }
+}
