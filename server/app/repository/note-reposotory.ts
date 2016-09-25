ae2ae77b6843ae6527476d7b439d519d4e304876
Andrea Zucchini <azga84@gmail.com>
2016-09-25 19:15:56 +0200

    Add note repository

---
+++ server/app/repository/note-reposotory.ts
@@ -0,0 +1,12 @@
+import {BaseRepository} from './base/base-repository';
+import {INote} from '../model/interfaces/note.interface';
+import {NoteModel} from '../model/note.model';
+
+export class NoteRepository extends BaseRepository<INote> {
+
+  constructor() {
+    super(NoteModel.getInstance());
+  }
+}
+
+Object.seal(NoteRepository);
