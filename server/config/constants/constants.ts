dd8bf044e7553116b0c51024df033fb75f567695
Andrea Zucchini <azga84@gmail.com>
2016-09-29 00:31:07 +0200

    add api secret

--- server/config/constants/constants.ts
+++ server/config/constants/constants.ts
@@ -1,5 +1,6 @@
 export default class Constants {
   static DB_CONNECTION_STRING: string = 'mongodb://localhost:27017/keepNotes';
   // Example: 'mongodb://db.your.domain:PORT/DB_NAME'
+  static API_SECRET: string = 'ilovesjavascript';
 }
 Object.seal(Constants);
