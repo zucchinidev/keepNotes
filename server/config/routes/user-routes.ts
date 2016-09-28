f6d4fe602e03e9a7b3f754037a2d2e0283249736
Andrea Zucchini <azga84@gmail.com>
2016-09-29 00:32:09 +0200

    Create user routes+

---
+++ server/config/routes/user-routes.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+import {UserController} from '../../app/controllers/user.controller';
+
+export class UserRoutes {
+  private router: express.Router;
+  constructor(private controller = new UserController()) {
+    this.router = express.Router();
+  }
+
+  getRoutes(): express.Router {
+    this.router.get('/', this.controller.getAll.bind(this.controller));
+    this.router.get('/:id', this.controller.findById.bind(this.controller));
+    this.router.post('/', this.controller.create.bind(this.controller));
+    this.router.put('/', this.controller.update.bind(this.controller));
+    this.router.patch('/', this.controller.patch.bind(this.controller));
+    this.router.delete('/:id', this.controller.remove.bind(this.controller));
+    this.router.post('/autenticate', this.controller.autenticate.bind(this.controller));
+    return this.router;
+  }
+}
+
