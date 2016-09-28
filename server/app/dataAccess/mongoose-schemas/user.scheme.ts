501cc78e9f8086e91630fe7b5ad844e767609c3c
Andrea Zucchini <azga84@gmail.com>
2016-09-29 00:35:44 +0200

    create user scheme

---
+++ server/app/dataAccess/mongoose-schemas/user.scheme.ts
@@ -0,0 +1,29 @@
+import * as mongoose from 'mongoose';
+import {BaseScheme} from './base.scheme';
+
+export class UserScheme extends BaseScheme {
+  private static getDefaulSchema(): Object {
+    return {
+      name: {
+        type: String,
+        required: true
+      },
+      password: {
+        type: String,
+        required: false
+      },
+      admin: {
+        type: Boolean,
+        required: true
+      }
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  get schema(): mongoose.Schema {
+    return UserScheme.createSchema(UserScheme.getDefaulSchema());
+  }
+}
