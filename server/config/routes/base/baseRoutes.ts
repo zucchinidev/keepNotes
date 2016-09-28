44912fffecf93cbf7d71587fe22321ac60b24c73
Andrea Zucchini <azga84@gmail.com>
2016-09-29 00:31:52 +0200

    add user routes

--- server/config/routes/base/baseRoutes.ts
+++ server/config/routes/base/baseRoutes.ts
@@ -1,12 +1,15 @@
 import * as express from 'express';
 import {NotesRoutes} from '../notes-routes';
+import {UserRoutes} from '../user-routes';
 
 let app = express();
 
 export class BaseRoutes {
   static routes(): express.Express {
     const notesRoutes = new NotesRoutes();
+    const userRoutes = new UserRoutes();
     app.use('/', notesRoutes.getRoutes());
+    app.use('/user', userRoutes.getRoutes());
     return app;
   }
 }
