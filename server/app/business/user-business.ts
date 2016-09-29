6f12f2be9643aec5c042c622781f9eca0bf21991
Andrea Zucchini <azga84@gmail.com>
2016-09-29 22:06:52 +0200

    Add domain logic for authenticate

--- server/app/business/user-business.ts
+++ server/app/business/user-business.ts
@@ -1,8 +1,16 @@
 import {IBaseBusiness} from './interfaces/base/base-business.interface';
 import {IUser} from '../model/interfaces/user.interface';
 import {UserRepository} from '../repository/user-repository';
+import * as jwt from 'jsonwebtoken';
+import Constants from '../../config/constants/constants';
 
 export class UserBusiness implements IBaseBusiness<IUser> {
+
+  private static getToken(user: IUser): string {
+    return jwt.sign(user, Constants.API_SECRET, {
+      expiresIn: 3600
+    });
+  }
 
   constructor(public repository: UserRepository = new UserRepository()) {}
 
@@ -57,6 +65,32 @@ export class UserBusiness implements IBaseBusiness<IUser> {
         .patch(_id, item)
         .then(resolve)
         .catch(reject);
+    });
+  }
+
+  authenticate(user: IUser): Promise<Object> {
+    return new Promise<Object>((resolve, reject) => {
+      this.repository
+        .findUser(user)
+        .then((userFound: IUser) => {
+          if (user.password !== userFound.password) {
+            resolve({
+              success: false,
+              message: 'Authentication failed. Wrong password.'
+            });
+          } else {
+            resolve({
+              success: true,
+              token: UserBusiness.getToken(user)
+            });
+          }
+        })
+        .catch(() => {
+          reject({
+            success: false,
+            message: 'Authentication failed. User not found.'
+          });
+        });
     });
   }
 }
