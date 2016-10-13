0d97e4d52aac7107aa66fd15e25e406086c23e63
Andrea Zucchini <azga84@gmail.com>
2016-10-13 19:54:01 +0200

    add signum method in auth controller

--- server/app/controllers/auth.controller.ts
+++ server/app/controllers/auth.controller.ts
@@ -9,6 +9,17 @@ export class AuthController extends BaseController {
   private authBusiness: AuthBusiness;
   private userBusiness: UserBusiness;
 
+  private static getToken(req: express.Request): any {
+    let token = req.body.token || req.query.token || req.headers['x-access-token'];
+    const authorization = req.headers['authorization'];
+    if (typeof authorization !== 'undefined' && authorization.indexOf('Bearer ') >= 0) {
+      const chunck = authorization.split(' ');
+      if (chunck[0] === 'Bearer') {
+        token =  chunck[1];
+      }
+    }
+    return token;
+  }
   constructor() {
     super();
     this.authBusiness = new AuthBusiness();
@@ -17,7 +28,7 @@ export class AuthController extends BaseController {
 
   verifyToken(req: express.Request, res: express.Response, next: NextFunction): void {
     try {
-      const token = req.body.token || req.query.token || req.headers['x-access-token'];
+      const token = AuthController.getToken(req);
       this.authBusiness.verifyToken(token)
         .then(response => {
           req['decoded'] = response['decoded'];
@@ -35,6 +46,17 @@ export class AuthController extends BaseController {
     try {
       const model = <IUser> req.body;
       this.userBusiness.authenticate(model)
+        .then(response => res.send(response))
+        .catch(err => res.send({error: err.message}));
+    } catch (err) {
+      AuthController.checkForError(err, res);
+    }
+  }
+
+  signup(req: express.Request, res: express.Response): void {
+    try {
+      const model = <IUser> req.body;
+      this.userBusiness.signup(model)
         .then(response => res.send(response))
         .catch(err => res.send({error: err.message}));
     } catch (err) {
