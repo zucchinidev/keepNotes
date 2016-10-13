6421ef4b56be2036d452afa9ec2f4124d133af2b
Andrea Zucchini <azga84@gmail.com>
2016-10-13 19:58:28 +0200

    route for index file

--- server/config/routes/base/base.routes.ts
+++ server/config/routes/base/base.routes.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import {NotesRoutes} from '../notes.routes';
 import {UserRoutes} from '../user.routes';
 import {AuthRoutes} from '../auth.routes';
+import * as path from 'path';
 
 export class BaseRoutes {
   static routes(): express.Express {
@@ -12,6 +13,13 @@ export class BaseRoutes {
     app.use('/', authRoutes.getRoutes());
     app.use('/api/note', notesRoutes.getRoutes());
     app.use('/api/user', userRoutes.getRoutes());
+    app.get('*', (req: express.Request, res: express.Response) => {
+      let pathIndex = '../../../../public/index.html';
+      let indexPath = path.resolve(__dirname, pathIndex);
+      console.log(indexPath);
+      res.set('Content-Type', 'text/html');
+      res.sendFile(indexPath, { root: __dirname });
+    });
     return app;
   }
 }
