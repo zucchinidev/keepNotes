ebd388a33d26afa9cb68e7d5710111aa4e09ab3d
Andrea Zucchini <azga84@gmail.com>
2016-10-13 19:56:02 +0200

    add static resources, set ejs view engine

--- server/app/main.ts
+++ server/app/main.ts
@@ -2,11 +2,23 @@ import * as mongoose from 'mongoose';
 import * as Promise from 'bluebird';
 import * as express from 'express';
 import  {Middlewares} from '../libs/middlewares/base/middlewares';
+import * as path from 'path';
+
 mongoose.Promise = Promise;
 
 const port = process.env.port || 8080;
 let app: express.Express = express();
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+let publicStatic = path.resolve(__dirname, '../../public');
+let vendorStatic = path.resolve(__dirname, '../../vendor');
+let clientStatic = path.resolve(__dirname, '../../client');
+let nodeModulesStatic = path.resolve(__dirname, '../../node_modules');
+app.use(express.static(publicStatic));
+app.use(express.static(vendorStatic));
+app.use(express.static(clientStatic));
+app.use(express.static(nodeModulesStatic));
 app.use(Middlewares.defaultConfiguration());
 
 app.listen(port, () => {
